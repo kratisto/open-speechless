e0e6988d97a9c2f0fa64f5135a034b1a83b265aa
Jeffrey Duroyon <jeffrey.duroyon@gmail.com>
2020-01-13 22:53:50 +0100

    feat(home): add homepage with vuetify

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      Open Speechless
+    </v-app-bar>
+
+    <v-content>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  props: {
+    source: String,
+  },
+  data: () => ({
+    drawer: null,
+  }),
+  components: {
+  },
+};
+</script>
